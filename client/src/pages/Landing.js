d3468cf5c76a29ec93e3ecbf2dee6c12d8b098a4
ashleyhkl <ho.k.leung@mail.mcgill.ca>
2024-12-14 04:18:49 -0500

    Landing still missing booking url

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,14 +1,25 @@
 import React from "react";
-import './Landing.css';
+import "./Landing.css";
 
+const Landing = () => {
 
-function Landing() {
-    return (
-    <div className="landing-page">
-      <img src="/logo.png" alt="logo"></img>
+  return (
+    <div className="layout-container">
+      <div className="main-content">
+        <div className="left-panel">
+          <img src="/logo.png" alt="Logo" className="logo-image" />
+        </div>
+        <div className="right-panel">
+          <h1>myBookings</h1>
+          <p>Book your first meeting now!</p>
+          <a className="start-button" href="/signup">Start Today!</a>
+          <p>Already a member? <a className="login-here" href="/login" >Login Here</a></p>
+          <p>Have a Booking URL?</p>
+        </div>
+      </div>
+     
     </div>
-    );
-  }
-  
-  export default Landing;
-  
+  );
+};
+
+export default Landing;
