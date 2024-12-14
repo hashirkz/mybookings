5555f3e95bbe801ed2aa0036ce742d52d43ef8a4
Ashley Leung <ho.k.leung@mail.mcgill.ca>
2024-12-13 22:40:51 -0500

    Added Signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <p>hi there</p>
-      <Login />
-    </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
   );
 }
 
