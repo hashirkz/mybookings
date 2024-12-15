abc63eb6e8761e5316749209801c85f230660bce
hashirkz <hashirxkhan1@gmail.com>
2024-12-15 07:09:56 +0000

    added GET /api/users/:user_id route to get username

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,6 +7,50 @@ import { mongo_conn } from "../mongodb.js";
 const ROUTER = express.Router();
 
 /*
+endpoint: /api/users/:user_id
+method: GET
+purpose: retrive the username *email* for a user from their user_id
+
+response_format:
+    {
+        status: "",
+        msg: "",
+        data: {
+            user: ""
+        }
+    }
+*/
+ROUTER.get("/:user_id", async(req, res) => {
+    try {
+        const user_id = req.params.user_id;
+        const conn = await mongo_conn();
+        const collection = conn.collection("users");
+
+        const data = await collection.findOne({ user_id: user_id});
+
+        if(!data) {
+            return res.status(404).json({
+                status: "error",
+                msg: `user ${user_id} does not exist`,
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            msg: "found user",
+            data: { user: data.user },
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            status: "error",
+            msg: "server error unable to retrieve user",
+        });
+    }
+});
+
+/*
+PROTECTED
 endpoint: /api/users/
 method: DELETE
 purpose: for deleting a logged in users account
